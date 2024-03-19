3d3979d16715c16ba2d45477424e69763e38e5cb
Roshini <roshini.bunga9@gmail.com>
2024-03-19 15:01:51 +0530

    Modified Java Application Directory Structure.

--- src/main/java/Addition.java
+++ src/main/java/Addition.java
@@ -11,9 +11,21 @@ public class Addition {
 		byte d = 4;
 		byte e = 5;
 		byte f = 6;
-		int result = a + b + c + d + e + f;
 
-		System.out.println(result);
+		int result1 = a + b + c + d + e + f;
+
+
+		byte g = 7;
+		byte h = 8;
+		byte i = 9;
+		byte j = 10;
+		byte k = 11;
+		byte l = 12;
+
+		int result2 = g + h + i + j + k + l;
+
+		System.out.println(result1);
+		System.out.println(result2);
 
 	}
 }
