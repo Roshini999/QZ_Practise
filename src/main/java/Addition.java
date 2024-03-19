dad8597a137b740f9d71fd099981d35841985e98
Roshini <roshini.bunga9@gmail.com>
2024-03-19 14:54:55 +0530

    Added Java Application Directory Structure.

---
+++ src/main/java/Addition.java
@@ -0,0 +1,19 @@
+package com.qualizeal.javaapp.mathematics;
+
+
+public class Addition {
+
+	public void add() {
+
+		byte a = 1;
+		byte b = 2;
+		byte c = 3;
+		byte d = 4;
+		byte e = 5;
+		byte f = 6;
+		int result = a + b + c + d + e + f;
+
+		System.out.println(result);
+
+	}
+}
---
+++ src/main/java/Program.java
@@ -0,0 +1,18 @@
+package com.qualizeal.javaapp.mathematics;
+
+
+import com.qualizeal.javaapp.mathematics.Addition;
+
+public class Program {
+
+	public static void main(String [] args) {
+
+			
+		Addition addition = new Addition();
+		addition.add();	
+
+
+	}
+	
+
+}
